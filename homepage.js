6fc3da000d170495bf8a820981e1089cf2f14729
Intellidevelopers <134317370+Intellidevelopers@users.noreply.github.com>
2023-11-19 06:26:14 +0100

    Create homepage.js

---
+++ homepage.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Homepage</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+    <h2>Welcome to the Homepage</h2>
+    <p>Your username is: <span id="usernameDisplay"></span></p>
+    <p>Your user ID is: <span id="userIdDisplay"></span></p>
+</div>
+
+<script src="https://www.gstatic.com/firebasejs/9.6.4/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.6.4/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.6.4/firebase-database.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.6.4/firebase-analytics.js"></script>
+<script src="homepage.js"></script>
+
+</body>
+</html>
